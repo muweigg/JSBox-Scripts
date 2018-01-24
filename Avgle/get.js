13ed7d3e276de7139b7babc6fc06349bd7fc572d
Wei Mu <[muweigg@gmail.com]>
2018-01-24 21:15:59 +0800

    update get hash

--- Avgle/get.js
+++ Avgle/get.js
@@ -1,14 +1,10 @@
-const url = encodeURI("https://avgle.com/video/99689/cmi-120-ゲスの極み映像-人妻25人目");
+const host = 'https://avgle.com';
+    url = encodeURI("https://avgle.com/video/99689/cmi-120-ゲスの極み映像-人妻25人目");
 // const url = encodeURI('https://avgle.com/video/130158/heyzo-1651');
-let kode = '';
 
-function buildHash (key, hash) {
-    return key + hash;
-}
-
-function decode(code) {
+function decode(code, hexs) {
     const c = code.split(' ');
-    let ohash = '', hash = '', ts = 0, vid = 0;
+    let kode = '', ohash = '', hash = '', ts = 0, vid = 0;
 
     for (var i = 0; i < c.length; i++) {
         kode += String.fromCharCode(parseInt(c[i]) - 3);
@@ -21,17 +17,14 @@ function decode(code) {
             kode = eval(kode);
     } catch (e) { }
 
-    kode = kode.match(/<source.*?>/)[0].match(/".*?"/g).map(function (v) {
-        if (!/video-info/.test(v))
-            return v.replace(/\\?"/g, '');
-    });
+    kode = kode.match(/<source.*?>/)[0].match(/".*?"/g)
+        .filter(v => !/video-info/.test(v))
+        .map(v => v.replace(/\\?"/g, ''));
 
-    ohash = $text.base64Decode((kode[1]));
-    ts = kode[2];
-    vid = kode[3];
+    ohash = $text.base64Decode((kode[0]));
+    ts = kode[1];
+    vid = kode[2];
 
-    const hexs = [0x2, 0x3, 0x5, 0x7, 0xb, 0xd, 0x11, 0x13, 0x17, 0x1d].reverse();
-    
     for (let i = 0; i < ohash.length; i++) {
         let ai = -1;
         for (let j = 0; j < hexs.length; j++) {
@@ -43,25 +36,69 @@ function decode(code) {
         }
         if (ai === -1) ai = i;
         charCode = ohash.charCodeAt(i) ^ ai;
-        hash = buildHash(String.fromCharCode(charCode), hash);
+        hash = String.fromCharCode(charCode) + hash;
     }
 
     hash = $text.base64Encode(hash);
     
     return { hash: hash, ts: ts, vid: vid };
 }
-$http.get({
-    url: url,
-    handler: function (resp) {
-        const data = resp.data;
-        const script = data.match(/<script\s*?id=.*?>[\s\S]*?<\/script>/)[0], code = script.match(/\\"([\d\s])*?\\"/)[0].match(/\\"(.*?)\\"/)[1];
-        const videoInfo = decode(code);
+
+async function getHexs (html) {
+
+    const keyScript = html.match(/\/templates.*avgle-main-ah\.js\?\d+/)[0];
+
+    return new Promise(resolve => {
+
         $http.get({
-            url: `https://avgle.com/video-url.php?hash=${videoInfo.hash}&ts=${videoInfo.ts}&vid=${videoInfo.vid}`,
+            url: `${host}${keyScript}`,
             handler: function(resp) {
-                var data = resp.data
-                $ui.alert(data.url);
+                let hexs = resp.data.match(/\$\(function[\s\S]*?var[\s\S]*?=(\[0x.*?\])/)[1];
+                hexs = eval(hexs).reverse();
+                resolve(hexs);
             }
-        })
-    }
-});
+        });
+
+    });
+}
+
+async function getHtml () {
+
+    return new Promise(resolve => {
+
+        $http.get({
+            url: url,
+            handler: function (resp) {
+                resolve(resp.data);
+            }
+        });
+
+    });
+}
+
+async function getVideoInfo () {
+    const html = await getHtml();
+    const hexs = await getHexs(html);
+
+    const script = html.match(/<script\s*?id=.*?>[\s\S]*?<\/script>/)[0],
+        code = script.match(/\\"([\d\s])*?\\"/)[0].match(/\\"(.*?)\\"/)[1];
+
+    const vInfo = decode(code, hexs);
+
+    return vInfo;
+}
+
+async function test () {
+    const vInfo = await getVideoInfo();
+    $console.info(JSON.stringify(vInfo, null, 2));
+    $http.get({
+        url: `${host}/video-url.php?hash=${vInfo.hash}&ts=${vInfo.ts}&vid=${vInfo.vid}`,
+        handler: function(resp) {
+            const data = resp.data
+            if (data.url) $share.sheet(data.url);
+            else $ui.alert('未获取到链接');
+        }
+    })
+}
+
+test();
