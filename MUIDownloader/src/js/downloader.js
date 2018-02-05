919ca29cf21669742d2b972e58f377f1e06c76e7
[muweigg] <muweigg@gmail.com>
2018-02-05 16:29:16 +0800

    update

--- MUIDownloader/src/js/downloader.js
+++ MUIDownloader/src/js/downloader.js
@@ -4,7 +4,7 @@
  * @description YouTube & Tumblr & Twitter & Facebook 视频下载器
  */
 
-let version = '2.0.0', link = '', keyword = '', rootView = '', rootWeb = '', platform = '';
+let version = '2.0.1', link = '', keyword = '', rootView = '', rootWeb = '', platform = '';
 
 link = $detector.link($context.text).map(link => {
     if (/youtu(\.?be)?|tumblr|twitter|facebook/.test(link))
